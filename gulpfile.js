4b34929fbf4047781bd1fa6f0f35b3f04767ac9d
suchhzz <jenjer444@gmail.com>
2025-03-21 23:06:36 +0200

    refactored .css compiling to one document, pages naming

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,38 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass')); // Используем Gulp-Sass и Sass
-const browserSync = require('browser-sync').create(); // Подключаем BrowserSync
+const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat'); 
+const browserSync = require('browser-sync').create(); 
 
-// Задача для компиляции SCSS в CSS
+// gulp.task('styles', function() {
+//   return gulp.src('scss/**/*.scss')
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(gulp.dest('css'))
+//     .pipe(browserSync.stream());
+// });
+
 gulp.task('styles', function() {
   return gulp.src('scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(concat('style.css'))
     .pipe(gulp.dest('css'))
-    .pipe(browserSync.stream()); // Обновляем браузер после компиляции стилей
+    .pipe(browserSync.stream());
 });
 
-// Задача для наблюдения за изменениями в SCSS файлах
 gulp.task('watch', function() {
-  gulp.watch('scss/**/*.scss', gulp.series('styles')); // Следим за всеми SCSS файлами в папке scss
-  gulp.watch('*.html').on('change', browserSync.reload); // Перезагружаем страницу при изменении HTML
-  gulp.watch('js/**/*.js').on('change', browserSync.reload); // Перезагружаем страницу при изменении JS
+  gulp.watch('scss/**/*.scss', gulp.series('styles'));
+  gulp.watch('*.html').on('change', browserSync.reload);
+  gulp.watch('js/**/*.js').on('change', browserSync.reload);
 });
 
-// Задача для запуска локального сервера с BrowserSync
 gulp.task('server', function(done) {
   browserSync.init({
     server: {
-      baseDir: './', // Корневая папка, откуда сервер будет обслуживать файлы
-      index: 'index.html' // Индексный файл (при необходимости)
+      baseDir: './',
+      index: 'index.html'
     },
-    notify: true // Показывать уведомление, когда сервер запускается
+    notify: true
   });
-  done(); // Завершаем задачу server
+  done();
 });
 
-// Задача по умолчанию
-gulp.task('default', gulp.series('styles', 'server', 'watch')); // Компилируем стили, запускаем сервер и начинаем отслеживание изменений
+gulp.task('default', gulp.series('styles', 'server', 'watch'));
