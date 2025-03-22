80c65c43ff37a42e604a3e1e1548555e5da348c3
suchhzz <jenjer444@gmail.com>
2025-03-22 11:25:03 +0200

    card page lower slider

--- js/product-list-menu.js
+++ js/product-list-menu.js
@@ -1,12 +1,24 @@
-$(document).ready(function() {
-    $('.product-list-menu-icon').click(function() {
+$(document).ready(function () {
+    $('.product-list-menu-icon').click(function () {
         $('.product-list-menu-icon').toggleClass('active');
         $('.product-list-menu').toggleClass('active');
     });
 
-    $('.product-list-item-arrow-down').click(function() {
+    $('.product-list-item-arrow-down').click(function () {
         let arrowData = $(this).data('menu');
         console.log(arrowData);
         $('#' + arrowData).toggleClass('active');
+    });
+
+    $('.slider-content .arrow-right').click(function () {
+        $('.slider').animate({
+            scrollLeft: '+=387'
+        }, 300);
+    });
+
+    $('.slider-content .arrow-left').click(function () {
+        $('.slider').animate({
+            scrollLeft: '-=387'
+        }, 300);
     });
 })
