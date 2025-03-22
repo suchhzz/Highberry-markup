2177d07db46cf78eb180f25741faa656a87008a4
suchhzz <jenjer444@gmail.com>
2025-03-22 11:38:27 +0200

    image slider

--- js/product-list-menu.js
+++ js/product-list-menu.js
@@ -1,3 +1,6 @@
+let imageSliderItemWidth = 439;
+let berrySliderItemWidth = 387
+
 $(document).ready(function () {
     $('.product-list-menu-icon').click(function () {
         $('.product-list-menu-icon').toggleClass('active');
@@ -12,13 +15,27 @@ $(document).ready(function () {
 
     $('.slider-content .arrow-right').click(function () {
         $('.slider').animate({
-            scrollLeft: '+=387'
+            scrollLeft: `+=${berrySliderItemWidth}`
         }, 300);
     });
 
     $('.slider-content .arrow-left').click(function () {
         $('.slider').animate({
-            scrollLeft: '-=387'
+            scrollLeft: `-=${berrySliderItemWidth}`
         }, 300);
     });
+
+    $('.main-image .arrow-right').click(function() {
+        $('.image-slider').animate({
+            scrollLeft: `+=${imageSliderItemWidth}`
+        }, 300);
+
+    });
+
+    $('.main-image .arrow-left').click(function() {
+        $('.image-slider').animate({
+            scrollLeft: `-=${imageSliderItemWidth}`
+        }, 300);
+    });
+
 })
