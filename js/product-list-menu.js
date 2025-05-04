d7f9a7e1cddef35b08f4ed1f50994d574931722c
suchhzz <jenjer444@gmail.com>
2025-05-04 14:37:36 +0300

    replaced img divider to html elements, header menu adaptive

--- js/product-list-menu.js
+++ js/product-list-menu.js
@@ -1,10 +1,16 @@
 $(document).ready(function () {
     $('.product-list-menu-icon').click(function () {
+        console.log('menu icon click');
         $('.product-list-menu-icon').toggleClass('active');
         $('.product-list-menu').toggleClass('active');
     });
 
-    $('.product-list-item-arrow-down').click(function () {
+    $('.product-list-menu').click(function (e) {
+        e.stopPropagation();
+    });
+
+    $('.product-list-item-arrow-down').click(function (e) {
+        e.stopPropagation();
         let arrowData = $(this).data('menu');
         console.log(arrowData);
         $('#' + arrowData).toggleClass('active');
@@ -22,14 +28,14 @@ $(document).ready(function () {
         }, 300);
     });
 
-    $('.main-image .arrow-right').click(function() {
+    $('.main-image .arrow-right').click(function () {
         $('.image-slider').animate({
             scrollLeft: `+=${$('.image-slider .slider-item').outerWidth(true)}`
         }, 300);
 
     });
 
-    $('.main-image .arrow-left').click(function() {
+    $('.main-image .arrow-left').click(function () {
         $('.image-slider').animate({
             scrollLeft: `-=${$('.image-slider .slider-item').outerWidth(true)}`
         }, 300);
